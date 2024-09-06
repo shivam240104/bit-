95f0602c8a83c97f2ef316af148e5957532a55bd
shivam240104 <mauryatanmay845@gmail.com>
2024-09-06 06:28:13 +0530

    set ith bit in java program

---
+++ setith.java
@@ -0,0 +1,11 @@
+public class setith {
+    
+    public static int set(int n,int i){
+        int bitmask=1<<i;
+        return n|bitmask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(set(10, 2));
+    }
+}
